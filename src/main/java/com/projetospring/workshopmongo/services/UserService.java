cb9561ea23c402b6e047a04c62509b97c1006f1e
Lucas Roberto de Oliveira Barcelos <lucasolbarcelos@gmail.com>
2022-05-19 12:55:45 -0300

    @LucassRoberto Task: tratamento de exceções

--- src/main/java/com/projetospring/workshopmongo/services/UserService.java
+++ src/main/java/com/projetospring/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.projetospring.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.projetospring.workshopmongo.domain.User;
 import com.projetospring.workshopmongo.repository.UserRepository;
+import com.projetospring.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,4 +19,9 @@ public class UserService {
 	public List<User> findAll(){
 		return repository.findAll(); //buscar todos os objetos do tipo user no banco de dados
 	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
 }
